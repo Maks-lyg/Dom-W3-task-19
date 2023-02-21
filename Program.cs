746a173fa3b3222a755ab2ccbaa96c637b573d97
Maks <maks-lug333@yandex.ru>
2023-02-21 20:15:01 +0900

    Work so far

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine(" Ввежите пятизначное число ->");
+string x = Console.ReadLine();
+if (x[0] == x[4] && x[1] == x[3])
+{
+    Console.WriteLine($"{x} -> Да");
+}
+else
+Console.WriteLine($"{x}-> Нет");
